b175b0967c8aa3de6a9c91d8e329658a2a3b04f3
VardanMd <madoyanvartan@yandex.ru>
2023-09-11 05:27:18 +0300

    KS-2-1 print data from second lesson

---
+++ src/main/kotlin/lesson_2_task_1/lesson_2_task_1.kt
@@ -0,0 +1,12 @@
+package lesson_2_task_1
+
+fun main() {
+    val studentNumber1 = 3
+    val studentNumber2 = 4
+    val studentNumber3 = 3
+    val studentNumber4 = 5
+
+    val averageScore: Float = (studentNumber1 + studentNumber2 + studentNumber3 + studentNumber4) / 4f
+
+    println(averageScore)
+}
